8c2bfac4a6b400b20775f5b4d6783e937419adfe
lívia souza <souzalivia8412@gmail.com>
2023-12-05 10:30:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import ToDoList from "./components/ToDoList"
+import './styles/styles.css'
+
+function App() {
+
+  return (
+ <div className="App">
+    <header>
+    <h2>Things To Do</h2>
+    <input type="text" 
+    autoComplete="off" 
+    placeholder="Escreva a tarefa" 
+    name="task" 
+    className="input"
+    />
+
+    <button>Adicionar</button>
+    </header>
+    <div className="line"></div>
+
+    <ToDoList />
+    </div>
+  )
+}
+
+export default App
